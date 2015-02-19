8eb3cd902ff2decc131f79d676140952202d1988
ragnar <ragnar@rmgk.de>
2015-02-19 15:41:17 +0100

    add grid

--- Benchmarks/src/main/scala/benchmarks/Grid.scala
+++ Benchmarks/src/main/scala/benchmarks/Grid.scala
@@ -1,5 +1,33 @@
 package benchmarks
 
-class Grid {
+import interface.ReactiveInterface
 
+import scala.collection.immutable.IndexedSeq
+import scala.collection.mutable
+
+
+case class Pos(x: Int, y: Int) {
+  def inBounds(target: Pos) = 0 <= x && 0 <= y && x < target.x && y < target.y
+}
+
+class Grid(val RI: ReactiveInterface, size: Pos, connections: Pos => List[Pos]) {
+
+  type Var = Grid.this.RI.IVar[String]
+  type Signal = Grid.this.RI.ISignal[String]
+  type Row = mutable.ArrayBuffer[Signal]
+
+  val sources: IndexedSeq[Var] = Range(0, size.x).map(i => RI.makeVar(f"$i"))
+
+  private val rows = {
+    val r = mutable.ArrayBuffer[Row]()
+    r += mutable.ArrayBuffer(sources: _*)
+    Range(1, size.y).foreach { y =>
+      r(y) = mutable.ArrayBuffer()
+      Range(0, size.x).foreach { x =>
+        val positions = connections(Pos(x, y)).filter(_.inBounds(Pos(x+1, y+1)))
+        val dependencies = positions.map(p => r(p.y)(p.x))
+        r(y) += RI.combineSeq(dependencies)(_.mkString(", "))
+      }
+    }
+  }
 }
