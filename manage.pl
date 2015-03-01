17d63e11b78ba138029de68831db9220f92f9122
ragnar <ragnar@rmgk.de>
2015-03-01 14:40:44 +0100

    improve manage script to select benchmark run

--- manage.pl
+++ manage.pl
@@ -11,13 +11,19 @@ use Data::Dumper;
 my $EXECUTABLE = './Benchmarks/target/start';
 my $OUTDIR = 'out';
 my $RESULTDIR = 'results';
+my @FRAMEWORKS = ("REScalaSpin", "REScalaSpinWait", "REScalaSTM", "REScalaSync", "SIDUP", "scala.rx", "scala.react");
 
 # stop java from formating numbers with `,` instead of `.`
 $ENV{'LANG'} = 'en_US.UTF-8';
 
+my $command = shift @ARGV;
+my @RUN = @ARGV ? @ARGV : qw< prim simple >;
 
-given($ARGV[0]) {
-  when (undef) { say Dumper(makeRuns()) }
+say "selected @RUN";
+
+
+given($command) {
+  when ("show") { say Dumper([ makeRuns() ]) }
   when ("init") { init() }
   when ("run") { run() }
   when ("submit") { submitAll() }
@@ -26,13 +32,14 @@ given($ARGV[0]) {
 sub init {
   mkdir $RESULTDIR;
   mkdir $OUTDIR;
+  mkdir "$RESULTDIR/$_" for @RUN;
   chdir "Benchmarks";
   system('sbt','clean', 'stage', 'compileJmh');
   chdir "..";
 }
 
 sub run {
-  my @runs = @{makeRuns()};
+  my @runs = makeRuns();
   for my $run (@runs) {
     my $prog = $run->{program};
     say "executing $prog";
@@ -40,13 +47,77 @@ sub run {
   }
 }
 
+sub submitAll {
+  for my $run (makeRuns()) {
+    submit(hhlrjob($run->{name}, $run->{program}))
+  }
+}
+
+sub submit {
+  my ($job) = @_;
+  open (my $BSUB, "|-", "bsub");
+  print $BSUB $job;
+  close $BSUB;
+}
+
 sub makeRuns {
+  my @runs;
+  for (@RUN) {
+    when ("simple") { push @runs, makeRunsSimple() }
+    when ("prim") { push @runs, makeRunsPrim() }
+  }
+  @runs;
+}
+
+sub makeRunString {
+  my ($prefix, $name, $args, @benchmarks) = @_;
+  my %arguments = %$args;
+  my %params = %{delete $arguments{p}};
+  my $paramstring =
+    (join ' ',
+      (map {"-$_ " . $arguments{$_}} keys %arguments),
+      (map {"-p $_=" . $params{$_}} keys %params),
+      (join " ", @benchmarks)
+    );
+  "$EXECUTABLE -rf csv -rff \"results/$prefix/$name.csv\" $paramstring"
+}
+
+sub makeRunsSimple {
   my @runs;
 
   for my $size (1..16,32,64) {
-    for my $framework ("REScalaSpin", "REScalaSpinWait", "REScalaSTM", "REScalaSync", "SIDUP", "scala.rx", "scala.react") {
+    for my $framework (@FRAMEWORKS) {
+      my $name = "threads_$size-framework_$framework";
+      my $program = makeRunString("simple", $name,
+        {
+          p => { # parameters
+            riname => $framework,
+          },
+          si => "false", # synchronize iterations
+          wi => 20, # warmup iterations
+          w => "1000ms", # warmup time
+          f => 5, # forks
+          i => 10, # iterations
+          r => "1000ms", # time per iteration
+          t => $size, #threads
+          to => "10s", #timeout
+        },
+        "simple.Mapping"
+      );
+      push @runs, {name => $name, program => $program};
+    }
+  }
+
+  @runs;
+}
+
+sub makeRunsPrim {
+  my @runs;
+
+  for my $size (1..16,32,64) {
+    for my $framework (@FRAMEWORKS) {
       my $name = "size_$size-framework_$framework";
-      my $program = makeRunString($name,
+      my $program = makeRunString("prim", $name,
         {
           p => { # parameters
             depth => $size * 4,
@@ -68,34 +139,9 @@ sub makeRuns {
     }
   }
 
-  \@runs;
+  @runs;
 }
 
-sub submitAll {
-  for my $run (@{ makeRuns() }) {
-    submit(hhlrjob($run->{name}, $run->{program}))
-  }
-}
-
-sub submit {
-  my ($job) = @_;
-  open (my $BSUB, "|-", "bsub");
-  print $BSUB $job;
-  close $BSUB;
-}
-
-sub makeRunString {
-  my ($name, $args, @benchmarks) = @_;
-  my %arguments = %$args;
-  my %params = %{delete $arguments{p}};
-  my $paramstring =
-    (join ' ',
-      (map {"-$_ " . $arguments{$_}} keys %arguments),
-      (map {"-p $_=" . $params{$_}} keys %params),
-      (join " ", @benchmarks)
-    );
-  "$EXECUTABLE -rf csv -rff \"results/$name.csv\" $paramstring"
-}
 
 
 sub hhlrjob {
@@ -106,10 +152,10 @@ sub hhlrjob {
 #BSUB -J REScalaBenchmark
 #
 # File / path where STDOUT will be written, the %J is the job id
-#BSUB -o $OUTDIR/$name.out
+#BSUB -o $OUTDIR/$name-%J.out
 #
 # File / path where STDERR will be written, the %J is the job id
-# #BSUB -e $OUTDIR/$name.err
+# #BSUB -e $OUTDIR/$name-%J.err
 #
 # Request the time you need for execution in minutes
 # The format for the parameter is: [hour:]minute,
